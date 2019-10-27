59969358e38f2c2ecf42796a87d51f940957ae2c
jbrennanjr <jtbrenna@gmail.com>
2019-10-27 14:15:21 -0400

    Adding some content to AboutUs, FAQ, and Home pages.

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,3 +1,27 @@
 <template>
-    <div>FAQ</div>
+    <div class="page-content">
+        <h3>Frequently Asked Questions</h3>
+        <br>
+        <p class="question">Is there a required dress code?</p>
+        <p class="question">When is the RSVP deadline?</p>
+        <p class="question">How do I RSVP?</p>
+        <p class="question">Where are the ceremony and the reception taking place?</p>
+        <p class="question">Will the ceremony and reception be indoors or outdoors?</p>
+        <p class="question">Are kids welcome?</p>
+        <p class="question">Do you have a hotel block for guests? Where do you recommend I stay?</p>
+        <p class="question">How do I get to the venue?</p>
+        <p class="question">Is there parking available near the ceremony and reception venue?</p>
+        <p class="question">What time should I arrive?</p>
+        <p class="question">If I still have questions, what is the best way to contact you?</p>
+    </div>
 </template>
+<style lang="less" scoped>
+.page-content {
+    padding: 20px;
+    text-align: left;
+
+    .question {
+        font-weight: bold;
+    }
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,19 @@
 <template>
-    <div>Home</div>
+    <div class="page-content">
+        <p>Welcome to our wedding website, we can’t wait to celebrate our wedding day with you.</p>
+        <br>
+        <p>We’ve created this website as a convenient and interactive way to share all of the important details with you in the lead up to our wedding.</p>
+        <br>
+        <p>If you have any questions at all for us regarding the wedding or any details, please feel free to call or text us, or email at <span class="email">jackbrennan.jessradin@gmail.com</span></p>
+    </div>
 </template>
+
+<style lang="less" scoped>
+.page-content {
+    padding: 20px;
+
+    .email {
+        color:#8c8cff;
+    }
+}
+</style>
