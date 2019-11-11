9edd821e88df07fd7145066b6a86a3d04e610f27
jbrennanjr <jtbrenna@gmail.com>
2019-11-10 20:58:59 -0500

    Adding photo component. Trying to figure out how to build list of paths

---
+++ src/components/Photos.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div
+            v-for="(src, index) in photos"
+            :key="index"
+            class="photo"
+            @click="() => showImg(index)"
+        ><img :src="src"></div>
+
+        <vue-easy-lightbox
+            :visible="visible"
+            :imgs="photos"
+            @hide="handleHide"
+        ></vue-easy-lightbox>
+    </div>
+</template>
+
+<script>
+import VueEasyLightbox from 'vue-easy-lightbox';
+
+export default {
+    components: {
+        VueEasyLightbox
+    },
+    data() {
+        return{
+            imageDir: "../assets/images/engagement/",
+            photos: [],
+            visible: false,
+            index: 0
+        }
+    },
+    methods: {
+        show() {
+            this.visible = true;
+        },
+        handleHide() {
+            this.visible = false;
+        },
+        showImg (index) {
+            this.index = index;
+            this.visible = true;
+        },
+        importAll(r) {
+            var photos = [];
+            r.keys().forEach(key => (photos[key] = r(key)));
+            this.photos = photos;
+        }
+    },
+    mounted() {
+         this.importAll(require.context(this.imageDir, true, /\.jpg$/));
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.photo {
+    img {
+        width: 20%;
+        cursor: pointer;
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Lightbox from 'vue-easy-lightbox'
 import App from './App.vue'
 import routes from './routes.js'
-import Lightbox from 'vue-easy-lightbox'
+import webpack from 'webpack';
 
 Vue.use(VueRouter);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(Lightbox);
+
+Vue.use(webpack);
 
 const router = new VueRouter({
     routes, 
